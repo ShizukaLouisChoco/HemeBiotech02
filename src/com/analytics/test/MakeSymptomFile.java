ac1dc41f34c0661029262d59bd8f3d087e8dcfd6
Shizuka Tanigawa <chocokkuma@gmail.com>
2022-05-29 18:04:36 +0200

    correction class names

--- src/com/analytics/test/MakeSymptomFile.java
+++ src/com/analytics/test/MakeSymptomFile.java
@@ -6,16 +6,16 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Map;
 
-public class SymptomFileMaker implements ISymptomFileMaker {
+public class MakeSymptomFile implements IMakeSymptomFile {
     // field
     private final String filepath;
 
     // constructor
-    public SymptomFileMaker(String filepath) {
+    public MakeSymptomFile(String filepath) {
         this.filepath = filepath;
     }
 
-    public void symptomFileMaker(Map<String, Integer> symptomData) {
+    public void makeSymptomFile(Map<String, Integer> symptomData) {
 
         try {
             FileWriter writer = new FileWriter(filepath);
